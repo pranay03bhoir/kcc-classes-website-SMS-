75b6c001616164eb8834fc7ef52a188d3dfe84b3
prana <pranaytb777@gmail.com>
2025-02-28 18:06:45 +0530

    Main server file.

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ const express = require("express");
 const connectToDb = require("./database/db.connection");
 const studentRoutes = require("./routes/student.routes");
 const teacherRoutes = require("./routes/teacher.routes");
+const adminRoutes = require("./routes/admin.routes");
 const app = express();
 
 app.use(express.json());
 connectToDb();
 app.use("/api/students", studentRoutes);
 app.use("/api/teachers", teacherRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running at Port ${process.env.PORT}`);
