805b56dd1248192e30d04a95968c90c227326c47
prana <pranaytb777@gmail.com>
2025-02-28 18:05:37 +0530

    Admin controller has all the admin features/privileges that an admin has.

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,5 +1,7 @@
 const Admin = require("../models/admin.model");
+const Student = require("../models/student.model");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const adminRegister = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -31,3 +33,68 @@ const adminRegister = async (req, res) => {
     });
   }
 };
+const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await Admin.findOne({ email: email });
+    if (!existingUser) {
+      res.status(400).json({
+        success: false,
+        message: `User is not registered, Please register first`,
+      })
+    }else {
+      const adminUser = await bcrypt.compare(password, existingUser.password);
+      if (adminUser) {
+        const payload = {
+          id: existingUser._id,
+          email: existingUser.email,
+          role: existingUser.role,
+        }
+        const accessToken = jwt.sign(payload,process.env.JWT_SECRET,{
+          expiresIn: "1h"
+        })
+        res.status(200).json({
+          success: true,
+          message: "Login successfully",
+          accessToken: accessToken,
+        })
+      }else {
+        res.status(401).json({
+          success: false,
+          message: "Login failed",
+        })
+      }
+    }
+
+  }catch (e) {
+    console.error(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+    })
+  }
+}
+const getAllStudents = async (req, res) => {
+  try {
+    const students = await Student.find({});
+    if (!students) {
+      res.status(404).json({
+        success: false,
+        message: "No students found",
+      })
+    }else {
+      res.status(200).json({
+        success: true,
+        message: "Students found",
+        students: students,
+      })
+    }
+  }catch (e) {
+    console.error(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+    })
+  }
+}
+module.exports = {adminRegister,adminLogin,getAllStudents}
