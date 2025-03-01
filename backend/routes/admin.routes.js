9d55e407e6b98f32ce49826d3642a88c6eb629c4
prana <pranaytb777@gmail.com>
2025-03-01 17:20:35 +0530

    Created the routes which grants full power to the admin.

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,12 +1,29 @@
-const express = require('express');
-const {adminRegister,adminLogin,getAllStudents} = require("../controllers/admin.controller")
-const adminAuth = require("../middlewares/adminAuth.middleware")
-const loginAuth = require("../middlewares/loginAuth.middleware")
+const express = require("express");
+const {
+  adminRegister,
+  adminLogin,
+  getAllStudents,
+  getAllTeachers,
+  getStudentsById,
+  getTeachersById,
+  updateTeachersDetails,
+  updateStudentsDetails,
+  deleteStudent,
+  deleteTeacher,
+} = require("../controllers/admin.controller");
+const adminAuth = require("../middlewares/adminAuth.middleware");
+const loginAuth = require("../middlewares/loginAuth.middleware");
 const router = express.Router();
 
-
-router.post("/register",adminRegister)
-router.post("/login",adminLogin)
-router.get("/students",loginAuth,adminAuth,getAllStudents)
+router.post("/register", adminRegister);
+router.post("/login", adminLogin);
+router.get("/students", loginAuth, adminAuth, getAllStudents);
+router.get("/teachers", loginAuth, adminAuth, getAllTeachers);
+router.get("/students/:id", loginAuth, adminAuth, getStudentsById);
+router.get("/teachers/:id", loginAuth, adminAuth, getTeachersById);
+router.put("/teachers/update/:id", loginAuth, adminAuth, updateTeachersDetails);
+router.put("/students/:id", loginAuth, adminAuth, updateStudentsDetails);
+router.delete("/teachers/:id", loginAuth, adminAuth, deleteTeacher);
+router.delete("/students/:id", loginAuth, adminAuth, deleteStudent);
 
 module.exports = router;
