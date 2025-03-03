0ed1a42b2de9309045e0e7894acec6b7aaa4dfb9
prana <pranaytb777@gmail.com>
2025-03-03 19:01:18 +0530

    updated course model

--- backend/models/course.model.js
+++ backend/models/course.model.js
@@ -4,7 +4,7 @@ const courseSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     code: { type: String, required: true, unique: true },
-    teacher: { type: mongoose.Schema.Types.ObjectId, ref: "Teacher" },
+    teachers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Teacher" }],
     students: [{ type: mongoose.Schema.Types.ObjectId, ref: "Student" }],
     createdAt: {
       type: Date,
