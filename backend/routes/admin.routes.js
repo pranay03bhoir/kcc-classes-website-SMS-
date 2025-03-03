1e6d11b26b0b5922582a8d2be82118c30e92c92a
prana <pranaytb777@gmail.com>
2025-03-03 19:01:35 +0530

    MERN - Student management system.

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -10,6 +10,15 @@ const {
   updateStudentsDetails,
   deleteStudent,
   deleteTeacher,
+  createCourse,
+  updateCourse,
+  getAllCourses,
+  deleteCourse,
+  removeStudentFromCourse,
+  getStudentsByCourse,
+  removeTeacherFromCourse,
+  markStudentAttendance,
+  getAttendanceRecords,
 } = require("../controllers/admin.controller");
 const adminAuth = require("../middlewares/adminAuth.middleware");
 const loginAuth = require("../middlewares/loginAuth.middleware");
@@ -18,6 +27,7 @@ const router = express.Router();
 router.post("/register", adminRegister);
 router.post("/login", adminLogin);
 router.get("/students", loginAuth, adminAuth, getAllStudents);
+router.get("/students/courses/:id", loginAuth, adminAuth, getStudentsByCourse);
 router.get("/teachers", loginAuth, adminAuth, getAllTeachers);
 router.get("/students/:id", loginAuth, adminAuth, getStudentsById);
 router.get("/teachers/:id", loginAuth, adminAuth, getTeachersById);
@@ -25,5 +35,22 @@ router.put("/teachers/update/:id", loginAuth, adminAuth, updateTeachersDetails);
 router.put("/students/:id", loginAuth, adminAuth, updateStudentsDetails);
 router.delete("/teachers/:id", loginAuth, adminAuth, deleteTeacher);
 router.delete("/students/:id", loginAuth, adminAuth, deleteStudent);
-
+router.post("/courses", loginAuth, adminAuth, createCourse);
+router.put("/courses/:id", loginAuth, adminAuth, updateCourse);
+router.get("/courses", loginAuth, adminAuth, getAllCourses);
+router.delete("/courses/:id", loginAuth, adminAuth, deleteCourse);
+router.put(
+  "/courses/students/:id",
+  loginAuth,
+  adminAuth,
+  removeStudentFromCourse,
+);
+router.put(
+  "/courses/teachers/:id",
+  loginAuth,
+  adminAuth,
+  removeTeacherFromCourse,
+);
+router.post("/students/attendance", markStudentAttendance);
+router.get("/all/attendance", getAttendanceRecords);
 module.exports = router;
