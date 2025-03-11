9d0daab8a3ef7691c6f87235498f4706678b02a1
prana <pranaytb777@gmail.com>
2025-03-11 16:23:25 +0530

    Add authentication middleware to student routes and implement update attendance feature

--- backend/routes/teacher.routes.js
+++ backend/routes/teacher.routes.js
@@ -6,14 +6,28 @@ const {
   getAllStudents,
   getStudentById,
   addStudentAttendance,
+  updateStudentAttendance,
 } = require("../controllers/teacher.controller");
+const loginAuth = require("../middlewares/loginAuth.middleware");
+const teacherAuth = require("../middlewares/teacherAuth.middleware");
 const router = express.Router();
 
 router.post("/register", teacherRegister);
 router.post("/login", teacherLogin);
 router.post("/logout", teacherLogout);
-router.get("/students", getAllStudents);
-router.get("/students/:studentId", getStudentById);
-router.post("/students/attendance", addStudentAttendance);
+router.get("/students", loginAuth, teacherAuth, getAllStudents);
+router.get("/students/:studentId", loginAuth, teacherAuth, getStudentById);
+router.post(
+  "/students/attendance",
+  loginAuth,
+  teacherAuth,
+  addStudentAttendance
+);
+router.put(
+  "/students/:studentId/attendance/:attendanceId",
+  loginAuth,
+  teacherAuth,
+  updateStudentAttendance
+);
 
 module.exports = router;
