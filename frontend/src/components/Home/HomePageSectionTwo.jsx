bf34cfb43a73c3a14d223dcd486b3f0f55c585ab
prana <pranaytb777@gmail.com>
2025-03-12 19:13:41 +0530

    Add HomePageSectionOne component with student registration form

---
+++ frontend/src/components/Home/HomePageSectionTwo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HomePageSectionTwo = () => {
+  return (
+    <div>
+      <div>
+        <h1 className={`text-4xl font-bold text-center t`}>
+          Why Choose Our Tutoring Program?
+        </h1>
+        <p className={`text-lg text-center pt-5 text-gray-700`}>
+          Empowering students with comprehensive education and personalized
+          attention
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HomePageSectionTwo;
