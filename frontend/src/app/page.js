0f1f2e8dfb290334a6307bcf5cdb484dcbcf5fe1
prana <pranaytb777@gmail.com>
2025-03-12 19:14:27 +0530

    Add Navbar and Home components to the main page layout

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,24 @@
 import Image from "next/image";
+import Navbar from "@/components/NavigationBar/NavBar";
+import HomeIntroScreen from "@/components/Home/HomeIntroScreen";
+import HomePageSectionOne from "@/components/Home/HomePageSectionOne";
+import HomePageSectionTwo from "@/components/Home/HomePageSectionTwo";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
+    <>
+      <div className="">
+        <Navbar />
+      </div>
+      <main className={`pt-40`}>
+        <HomeIntroScreen />
+      </main>
+      <section className={`pt-16`}>
+        <HomePageSectionOne />
+      </section>
+      <section className={`pt-16`}>
+        <HomePageSectionTwo />
+      </section>
+    </>
   );
 }
