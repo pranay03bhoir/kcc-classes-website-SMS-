8ae3d7003249f798f659111a36c63d09a8157e88
prana <pranaytb777@gmail.com>
2025-03-14 16:54:56 +0530

    Add InfoCards component for displaying information with icons

---
+++ frontend/src/components/CardComponent/InfoCards.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const InfoCards = ({ icon: Icon, title, description }) => {
+  const CommonClassesForTheCardsIcons =
+    "bg-neutral-300 w-fit p-3 rounded-full mb-5";
+  const CommonClassesForCards =
+    "rounded-lg p-5 hover:shadow-md transition duration-300 bg-[#FBFAFB] ease-in-out";
+  return (
+    <div className={`${CommonClassesForCards}`}>
+      <div className={`${CommonClassesForTheCardsIcons}`}>
+        <Icon />
+      </div>
+      <h1 className="text-xl font-bold mb-2.5">{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default InfoCards;
