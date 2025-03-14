d3e726544e898d8efbd1824bb9d859e1081b06b6
prana <pranaytb777@gmail.com>
2025-03-14 16:54:32 +0530

    Add HomePageSectionFour component to showcase unique features

---
+++ frontend/src/components/Home/HomePageSectionFour.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePageSectionFour = () => {
+  return (
+    <div>
+      <h1 className={``}>What Makes Us Different</h1>
+      <p>Experience excellence in education with our unique features.</p>
+    </div>
+  );
+};
+
+export default HomePageSectionFour;
