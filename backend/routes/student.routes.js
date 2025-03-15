f9b9795decd9f8efd85bb78d3a3321f6f3b51f40
prana <pranaytb777@gmail.com>
2025-03-15 18:06:00 +0530

    Add student logout and profile update routes with authentication middleware

--- backend/routes/student.routes.js
+++ backend/routes/student.routes.js
@@ -2,9 +2,14 @@ const express = require("express");
 const {
   studentRegister,
   studentLogin,
+  updateStudentProfile,
+  studentLogout,
 } = require("../controllers/student.controller");
+const loginAuth = require("../middlewares/loginAuth.middleware.js");
 const router = express.Router();
 
 router.post("/register", studentRegister);
 router.post("/login", studentLogin);
+router.post("/logout", studentLogout);
+router.put("/update", loginAuth, updateStudentProfile);
 module.exports = router;
