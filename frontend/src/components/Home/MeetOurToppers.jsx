bd5342143ff75a407b50ffc5881bcf2f8bcb3d8e
prana <pranaytb777@gmail.com>
2025-03-15 15:45:36 +0530

    Add MeetOurToppers component to showcase student success stories

---
+++ frontend/src/components/Home/MeetOurToppers.jsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import TopperCards from "@/components/CardComponent/TopperCards";
+const MeetOurToppers = () => {
+  return (
+    <div>
+      <div
+        className={`text-center flex md:flex-row flex-col md:justify-center gap-5 items-center`}
+      >
+        <h1 className={`text-xl`}>Inspiring Success Stories </h1>
+        <span className={`text-4xl hidden md:block`}>|</span>
+        <h1 className={`text-4xl`}>Our Toppers</h1>
+      </div>
+      <div className={`pt-10 grid md:grid-cols-4 gap-10`}>
+        <TopperCards />
+        <TopperCards />
+        <TopperCards />
+        <TopperCards />
+        <TopperCards />
+        <TopperCards />
+        <TopperCards />
+        <TopperCards />
+      </div>
+    </div>
+  );
+};
+
+export default MeetOurToppers;
