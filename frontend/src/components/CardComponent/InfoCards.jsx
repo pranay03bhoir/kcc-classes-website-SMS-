6b97eceaf15d64680623f867e4517eecbb95acfd
prana <pranaytb777@gmail.com>
2025-03-15 15:45:15 +0530

    Refactor InfoCards component structure for improved layout

--- frontend/src/components/CardComponent/InfoCards.jsx
+++ frontend/src/components/CardComponent/InfoCards.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 
 const InfoCards = ({ icon: Icon, title, description }) => {
@@ -6,12 +7,14 @@ const InfoCards = ({ icon: Icon, title, description }) => {
   const CommonClassesForCards =
     "rounded-lg p-5 hover:shadow-md transition duration-300 bg-[#FBFAFB] ease-in-out";
   return (
-    <div className={`${CommonClassesForCards}`}>
-      <div className={`${CommonClassesForTheCardsIcons}`}>
-        <Icon />
+    <div>
+      <div className={`${CommonClassesForCards}`}>
+        <div className={`${CommonClassesForTheCardsIcons}`}>
+          <Icon />
+        </div>
+        <h1 className="text-xl font-bold mb-2.5">{title}</h1>
+        <p>{description}</p>
       </div>
-      <h1 className="text-xl font-bold mb-2.5">{title}</h1>
-      <p>{description}</p>
     </div>
   );
 };
