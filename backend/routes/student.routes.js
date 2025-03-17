b686d994ce785cdc6c09388901a9078a6388d68d
prana <pranaytb777@gmail.com>
2025-03-17 19:24:37 +0530

    Add endpoints for course retrieval and access token regeneration in student routes

--- backend/routes/student.routes.js
+++ backend/routes/student.routes.js
@@ -4,12 +4,16 @@ const {
   studentLogin,
   updateStudentProfile,
   studentLogout,
+  getStudentCourses,
+  generateNewRefreshAccessToken,
 } = require("../controllers/student.controller");
 const loginAuth = require("../middlewares/loginAuth.middleware.js");
 const router = express.Router();
 
 router.post("/register", studentRegister);
 router.post("/login", studentLogin);
+router.post("/refresh", generateNewRefreshAccessToken);
 router.post("/logout", studentLogout);
 router.put("/update", loginAuth, updateStudentProfile);
+router.get("/get/courses", loginAuth, getStudentCourses);
 module.exports = router;
