56c86d0dbe3520db40002f7efe94b18bb8f2ffd5
prana <pranaytb777@gmail.com>
2025-03-19 16:23:17 +0530

    Remove unused toast import from GetInTouchForm component

--- frontend/src/components/FormComponent/GetInTouchForm.jsx
+++ frontend/src/components/FormComponent/GetInTouchForm.jsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-// import { toast } from "sonner";
 import toast, { Toaster } from "react-hot-toast";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
