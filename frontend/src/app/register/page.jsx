2558faf81ededc0f434c18979d6d17cb6261d9a8
prana <pranaytb777@gmail.com>
2025-03-19 19:11:31 +0530

    Add RegisterForm component to handle user registration

---
+++ frontend/src/app/register/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Register from "@/components/LoginComponent/Register";
+
+const RegisterForm = () => {
+  return (
+    <div className={`bg-black`}>
+      <Register />
+    </div>
+  );
+};
+
+export default RegisterForm;
