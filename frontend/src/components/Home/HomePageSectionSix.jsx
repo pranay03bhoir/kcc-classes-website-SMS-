a347ca097713550e8b469a921477584306da3e25
prana <pranaytb777@gmail.com>
2025-03-19 16:22:28 +0530

    Add HomePageSectionSix component with GetInTouchForm integration

---
+++ frontend/src/components/Home/HomePageSectionSix.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import GetInTouchForm from "@/components/FormComponent/GetInTouchForm";
+
+const HomePageSectionSix = () => {
+  return (
+    <div className={`bg-[#F9F9F9] py-16`}>
+      <div>
+        <h1 className={`text-4xl font-bold text-center`}>Get in Touch</h1>
+      </div>
+      <GetInTouchForm />
+    </div>
+  );
+};
+
+export default HomePageSectionSix;
