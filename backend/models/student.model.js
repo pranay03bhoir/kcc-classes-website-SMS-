8dcd1f0ead8ccd837e1df737c3801e98174ac566
prana <pranaytb777@gmail.com>
2025-03-23 18:36:46 +0530

    Add studentId field and pre-save hook for automatic ID generation in student model

--- backend/models/student.model.js
+++ backend/models/student.model.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-
+const Counter = require("./counterStudent.model");
 const studentSchema = new mongoose.Schema(
   {
+    studentId: { type: String, unique: true, index: true },
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, index: true },
     password: { type: String, required: true },
     contact: { type: String, required: true },
     parentsContact: [{ type: String, required: true }],
@@ -13,6 +14,7 @@ const studentSchema = new mongoose.Schema(
     isVerified: { type: Boolean, default: false },
     profileImage: { type: String, default: "" }, // Stored in AWS S3
     courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+    batches: [{ type: mongoose.Schema.Types.ObjectId, ref: "Batch" }],
     attendance: [
       {
         type: mongoose.Schema.Types.ObjectId,
@@ -26,6 +28,7 @@ const studentSchema = new mongoose.Schema(
       },
     ],
     refreshToken: { type: String, default: "" },
+    admissionYear: { type: Number, required: true },
     createdAt: {
       type: Date,
       default: Date.now,
@@ -37,4 +40,20 @@ const studentSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+studentSchema.pre("save", async function (next) {
+  if (!this.studentId) {
+    const year = this.admissionYear;
+    const counter = await Counter.findOneAndUpdate(
+      {
+        name: `Student-${year}`,
+      },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true, runValidators: true },
+    );
+    this.studentId = `STU-${year}-${String(counter.seq).padStart(4, "0")}`;
+  }
+  next();
+});
+
 module.exports = mongoose.model("Student", studentSchema);
