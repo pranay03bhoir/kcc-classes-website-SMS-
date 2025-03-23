c855c6b9834ff2ff8b12901ab7b9e67313ba5272
prana <pranaytb777@gmail.com>
2025-03-23 18:36:29 +0530

    Add batchId generation and update batch model schema for improved structure

--- backend/models/batch.model.js
+++ backend/models/batch.model.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
-
+const Counter = require("./batchCounter.model");
 const batchSchema = new mongoose.Schema({
+  batchId: { type: String, unique: true, index: true },
   name: { type: String, required: true },
-  class: { type: String, required: true },
+  classStd: { type: String, required: true },
+  timings: { type: String, required: true },
   courseId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Course",
@@ -11,9 +13,23 @@ const batchSchema = new mongoose.Schema({
   teacherId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Teacher",
-    required: true,
   },
   studentIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Student" }],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+batchSchema.pre("save", async function (next) {
+  if (!this.batchId) {
+    const timing = this.timings;
+    const std = this.classStd;
+    const counter = await Counter.findOneAndUpdate(
+      { name: `Batch-${std}` },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true },
+    );
+    this.batchId = `BTH-${std}-${timing}-${String(counter.seq).padStart(4, "0")}`;
+  }
 });
 
 module.exports = mongoose.model("Batch", batchSchema);
