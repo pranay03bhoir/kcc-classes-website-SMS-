c94f480a4b6304963c1b0cf23555d07bf0540a40
prana <pranaytb777@gmail.com>
2025-03-23 18:36:55 +0530

    Add teacherId field and pre-save hook for automatic ID generation in teacher model

--- backend/models/teacher.model.js
+++ backend/models/teacher.model.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-
+const Counter = require("./counterTeacher.model");
 const teacherSchema = new mongoose.Schema(
   {
+    teacherId: { type: String, unique: true, index: true },
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, index: true },
     password: { type: String, required: true },
     contact: { type: String, required: true },
     alternateContact: { type: String, required: true },
@@ -13,6 +14,8 @@ const teacherSchema = new mongoose.Schema(
     profileImage: { type: String, default: "" },
     courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
     refreshToken: { type: String, default: "" },
+    joiningYear: { type: Number, required: true },
+    batches: [{ type: mongoose.Schema.Types.ObjectId, ref: "Batch" }],
     createdAt: {
       type: Date,
       default: Date.now,
@@ -24,4 +27,17 @@ const teacherSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+teacherSchema.pre("save", async function (next) {
+  if (!this.teacherId) {
+    const year = this.joiningYear;
+    const counter = await Counter.findOneAndUpdate(
+      { name: `teacher-${year}` },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true },
+    );
+    this.teacherId = `Teacher-${year}-${String(counter.seq).padStart(4, "0")}`;
+  }
+});
+
 module.exports = mongoose.model("Teacher", teacherSchema);
