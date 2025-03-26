5e1dc67995799de64c8f445de6693ab26bcce923
prana <pranaytb777@gmail.com>
2025-03-26 19:43:00 +0530

    Remove redundant call-to-action section from HomePageSectionSix component

--- frontend/src/components/Home/HomePageSectionSix.jsx
+++ frontend/src/components/Home/HomePageSectionSix.jsx
@@ -10,19 +10,6 @@ const HomePageSectionSix = () => {
         <h1 className={`text-4xl font-bold text-center`}>Get in Touch</h1>
       </div>
       <GetInTouchForm />
-      <div className={`mx-auto`}>
-        <h1 className={`text-2xl text-center pt-16 font-bold`}>OR</h1>
-        <div className={`mx-auto`}>
-          <a
-            href={`tel:+918830986365`}
-            className={`w-full flex justify-center items-center px-2 pt-16`}
-          >
-            <Button className={`h-12 text-xl w-48 bg-green-700`}>
-              Call us <IoCall />
-            </Button>
-          </a>
-        </div>
-      </div>
     </div>
   );
 };
