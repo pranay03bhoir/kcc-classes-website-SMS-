56181a1f21604dd64ca89e95ee4c8f33b9191f16
prana <pranaytb777@gmail.com>
2025-03-27 18:56:32 +0530

    Add Home component with multiple sections and navigation

---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Navbar from "@/components/NavigationBar/NavBar";
+import HomeIntroScreen from "@/components/Home/HomeIntroScreen";
+import HomePageSectionOne from "@/components/Home/HomePageSectionOne";
+import HomePageSectionTwo from "@/components/Home/HomePageSectionTwo";
+import HomePageSectionThree from "@/components/Home/HomePageSectionThree";
+import HomePageSectionFour from "@/components/Home/HomePageSectionFour";
+import AboutOurInstitution from "@/components/Home/AboutOurInstitution";
+import MeetOurToppers from "@/components/Home/MeetOurToppers";
+import HomePageSectionFive from "@/components/Home/HomePageSectionFive";
+import HomePageSectionSix from "@/components/Home/HomePageSectionSix";
+import Footer from "@/components/Home/Footer";
+const Home = () => {
+  return (
+    <div>
+      <div className="">
+        <Navbar />
+      </div>
+      <main className={`pt-40`}>
+        <HomeIntroScreen />
+      </main>
+      <section className={`pt-16`}>
+        <HomePageSectionOne />
+      </section>
+      <section className={`pt-16`}>
+        <AboutOurInstitution />
+      </section>
+      <section className={`pt-16`}>
+        <HomePageSectionTwo />
+      </section>
+      <section className={`pt-32`}>
+        <HomePageSectionThree />
+      </section>
+      <section className={`pt-32`}>
+        <HomePageSectionFour />
+      </section>
+      <section className={`pt-32`}>
+        <MeetOurToppers />
+      </section>
+      <section className={`pt-32`}>
+        <HomePageSectionFive />
+      </section>
+      <section className={`pt-32`}>
+        <HomePageSectionSix />
+      </section>
+      <section>
+        <Footer />
+      </section>
+    </div>
+  );
+};
+
+export default Home;
