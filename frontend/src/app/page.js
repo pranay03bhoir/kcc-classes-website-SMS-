7e8b61bc8dab835c6cd2f03301511f194d513949
prana <pranaytb777@gmail.com>
2025-03-30 19:46:30 +0530

    Rename Home component to HomePage for consistency in imports

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import HomePage from "@/components/Home/HomePage";
 
 export default function Home() {
   return (
     <div className={`overflow-x-hidden`}>
-      <Home />
+      <HomePage />
     </div>
   );
 }
