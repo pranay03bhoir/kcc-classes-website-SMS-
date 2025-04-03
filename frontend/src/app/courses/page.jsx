251fa6a4f697eb22958b8cd9ee70ad556b16b810
prana <pranaytb777@gmail.com>
2025-04-03 17:42:57 +0530

    Add Page component to render Courses component

---
+++ frontend/src/app/courses/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Courses from "@/components/CoursesExploration/Courses";
+
+const Page = () => {
+  return (
+    <div>
+      <Courses />
+    </div>
+  );
+};
+
+export default Page;
