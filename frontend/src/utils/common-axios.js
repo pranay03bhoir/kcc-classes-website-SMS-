36a07d924dc3ca57efaa18a0f9c25b255e5ab1f5
prana <pranaytb777@gmail.com>
2025-06-25 18:55:09 +0530

    chore: simplify API base URL configuration by directly using the environment variable for better clarity and maintainability

--- frontend/src/utils/common-axios.js
+++ frontend/src/utils/common-axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_AXIOS_USER_URL ||
-    "http://localhost:5000/api/common", // Replace with your backend URL
+  baseURL: process.env.NEXT_PUBLIC_AXIOS_USER_URL, // Replace with your backend URL
   withCredentials: true, // This ensures that cookies are sent with requests
   headers: {
     "Content-Type": "application/json",
